c2f61e0786d18382a323aff490f92041079c8a24
Eric Hubbard <rehubbard2@gmail.com>
2019-12-06 10:24:01 -0600

    Refactored code to be DRY using private class method and before_action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,13 @@
-class ArticlesController < ApplicationController 
+class ArticlesController < ApplicationController
+  before_action  :set_article, only: [:edit, :update, :show, :destroy]
+  def index
+    @articles = Article.all
+  end
   def new
     @article = Article.new
+  end
+
+  def edit
   end
 
   def create
@@ -15,13 +22,31 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def update
+    if @article.update(article_params)
+      flash[:notice] = "Article was successfully updated"
+      redirect_to article_path(@article)
+    else 
+      # update article failed, showing error flash message in application layout view
+      render 'edit'
+    end
+  end
+
   def show
-    @article = Article.find(params[:id])
+  end
+
+  def destroy
+    @article.destroy
+    flash[:notice] = "Article deleted!"
+    redirect_to articles_path
   end
 
 
 
   private 
+    def set_article
+      @article = Article.find(params[:id])
+    end
     def article_params
       params.require(:article).permit(:title, :description) 
     end
